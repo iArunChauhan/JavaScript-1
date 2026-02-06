cc0b64cb9ae6dc9c627ba8f916cdde67b48fc262
iArunChauhan <arunkumarak1528363@gmail.com>
2026-02-06 13:26:21 +0530

    UI design

---
+++ js-11/script.js
@@ -0,0 +1,11 @@
+
+
+let searchInpu = document.getElementById("searchInput")
+
+let result = document.getElementById("results")
+
+let errors = document.getElementById("erro") ;
+
+let loader = document.getElementById("Loader")
+
+
